96c1f7ddfa5b9d562e06b67ee1d5b4a019f1dc0d
DukeOfEtiquette <duquetteadam@gmail.com>
2019-07-22 11:58:48 -0700

    Actually add the basic home screen functionallity with styling in the codebase, not just snack.expo.io : /

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,16 +3,39 @@ import {
   StyleSheet,
   Text,
   View,
+  Button,
+  TextInput,
 } from 'react-native';
+import * as Speech from 'expo-speech';
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <MonoText style={styles.message}>Hello, ...</MonoText>
-    </View>
-  );
+export default class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '...'
+    }
+  }
+
+  speak = () => {
+    Speech.speak(this.state.name);
+  }
+
+  onNameChange = (name) => {
+    console.log(this.state.name)
+    this.setState({name});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Press to hear some words" onPress={this.speak} />
+        <Text style={styles.name}>Hello, {this.state.name} </Text>
+        <TextInput value={this.state.name} style={styles.inputField} onChangeText={this.onNameChange} />
+      </View>
+    );
+  }
 }
 
 HomeScreen.navigationOptions = {
@@ -22,12 +45,19 @@ HomeScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     justifyContent: 'center',
+    flexDirection: 'column',
     backgroundColor: '#ccc',
+    padding: 8,
   },
-  message: {
+  name: {
     fontSize: 30,
     color: '#19f',
+  },
+  inputField: {
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1,
+    color: '#999'
   }
 });
