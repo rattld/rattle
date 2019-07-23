bb71bd2a306d79c489a8e41db3dcb806de14c2fe
Calvin Cheng <calvincheng919@gmail.com>
2019-07-23 11:30:47 -0700

    Header component, change to Speak

--- screens/SpeakScreen.js
+++ screens/SpeakScreen.js
@@ -1,34 +1,60 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
 import * as Speech from 'expo-speech';
+import Header from '../components/Header';
+import { DeviceMotion } from 'expo-sensors';
+
 
 import { NameContext } from '../context/nameContext';
 
-export default function ShakeScreen() {
+export default function SpeakScreen() {
   
   let context = useContext(NameContext);
+  const [dm, setDM] = useState(null);
 
   speak = ()=> {
     let message = `Hello ${context.name}, how are you?`;
     Speech.speak(message);
   }
 
-  useEffect(() => this.speak(), []);
+  // useEffect(() => this.speak(), []);
+  useEffect(() => {
+    speak();
+    DeviceMotion.addListener( motion => {
+      setDM(motion);
+    });
+  });
+
+  handleDeviceMotion = (e)=>{
+    speak();
+  }
+
+  // DeviceMotion.setUpdateInterval(100)
+  // DeviceMotion.addListener(handleDeviceMotion)
+
+  // if(dm) {
+
+  // }
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
+      <View><Header /></View>
+
       <View style={{width: 50, height: 100, backgroundColor: '#444'}} />
-      <Text style={styles.bodyText}>Hello {context.name}, you are on the ShakeScreen</Text>
+        <Text style={styles.bodyText}>Hello {context.name}, you are on the SpeakScreen</Text>
       <View style={{width: 50, height: 50, backgroundColor: '#444'}} />
+
       <View style={styles.buttonStyle}>
         <Button onPress ={speak} title="Press Me Yo!" color="black" />
+        <Text>{dm? dm.acceleration.x: 'nothing'}</Text>
       </View>
-    </ScrollView>
+
+    </View>
   );
 }
 
-ShakeScreen.navigationOptions = {
-  title: 'Shakes',
+SpeakScreen.navigationOptions = {
+  title: 'Speaks',
 };
 
 const styles = StyleSheet.create({
